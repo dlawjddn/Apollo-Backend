d877ec6e9d478e2ae9ad564ae253ee69182e210c
kurtyoon <akakertcobain@naver.com>
2023-06-21 22:15:15 +0900

    FEAT test용 Controller 작성

---
+++ src/main/java/com/Teletubbies/Apollo/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.Teletubbies.Apollo.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloWorldController {
+    
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
