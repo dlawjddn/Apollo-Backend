790077fb236f6d523b9001b874ebdc6be2e0b3f5
kurtyoon <akakertcobain@naver.com>
2023-06-27 16:21:58 +0900

    FEAT apollo 데이버테이스에 user정보 저장

--- src/main/java/com/Teletubbies/Apollo/auth/service/UserService.java
+++ src/main/java/com/Teletubbies/Apollo/auth/service/UserService.java
@@ -6,12 +6,14 @@ import com.Teletubbies.Apollo.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
     public void saveUser(MemberInfoResponse memberInfoResponse) {
-        userRepository.save(new User(memberInfoResponse));
+        userRepository.save(memberInfoResponse.changeDTOtoObj(memberInfoResponse));
     }
 }
