7ed65427c5b058c8f9de4426fe0d1fe2d0514970
dlawjddn <jwl5015@naver.com>
2023-07-02 17:13:30 +0900

    FEAT: 레포 정보 dto 변환 성공

--- src/main/java/com/Teletubbies/Apollo/auth/controller/AccessTokenController.java
+++ src/main/java/com/Teletubbies/Apollo/auth/controller/AccessTokenController.java
@@ -2,6 +2,7 @@ package com.Teletubbies.Apollo.auth.controller;
 
 import com.Teletubbies.Apollo.auth.domain.User;
 import com.Teletubbies.Apollo.auth.dto.MemberInfoResponse;
+import com.Teletubbies.Apollo.auth.dto.RepoInfoResponse;
 import com.Teletubbies.Apollo.auth.repository.UserRepository;
 import com.Teletubbies.Apollo.auth.service.OAuthService;
 import com.Teletubbies.Apollo.auth.service.UserService;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -39,7 +41,7 @@ public class AccessTokenController {
     @GetMapping("/repository/list")
     public String getRepoList() throws ParseException {
         User user = userRepository.findByLogin("dlawjddn").get();
-        ResponseEntity<String> repoURL = oAuthService.getRepoURL(user);
+        List<RepoInfoResponse> repoURL = oAuthService.getRepoURL(user);
         return "repo list success";
     }
 }
