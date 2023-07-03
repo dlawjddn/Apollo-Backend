feac379d3147d635883877af4300b08ced4d2865
dlawjddn <jwl5015@naver.com>
2023-07-03 21:06:46 +0900

    FEAT: repo 조회 로직 추가

--- src/main/java/com/Teletubbies/Apollo/auth/repository/RepoRepository.java
+++ src/main/java/com/Teletubbies/Apollo/auth/repository/RepoRepository.java
@@ -3,5 +3,11 @@ package com.Teletubbies.Apollo.auth.repository;
 import com.Teletubbies.Apollo.auth.domain.Repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RepoRepository extends JpaRepository<Repo, Long> {
+    List<Repo> findByOwnerLogin (String login);
+    Optional<Repo> findByRepoName (String name);
+
 }
