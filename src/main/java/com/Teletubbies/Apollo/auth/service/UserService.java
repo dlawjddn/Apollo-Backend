10d81dff4ea6ea8eb9a797de4196d687651fb841
dlawjddn <jwl5015@naver.com>
2023-07-05 11:48:14 +0900

    FEAT: custom error 추가, userService에 적용

--- src/main/java/com/Teletubbies/Apollo/auth/service/UserService.java
+++ src/main/java/com/Teletubbies/Apollo/auth/service/UserService.java
@@ -3,10 +3,13 @@ package com.Teletubbies.Apollo.auth.service;
 import com.Teletubbies.Apollo.auth.domain.User;
 import com.Teletubbies.Apollo.auth.dto.MemberInfoResponse;
 import com.Teletubbies.Apollo.auth.repository.UserRepository;
+import com.Teletubbies.Apollo.core.exception.ApolloException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static com.Teletubbies.Apollo.core.exception.CustomErrorCode.INVALID_USER_ERROR;
 
 @Service
 @RequiredArgsConstructor
@@ -18,17 +21,17 @@ public class UserService {
     public User getUserById(Long id){
         Optional<User> optionalUser = userRepository.findById(id);
         if (optionalUser != null && optionalUser.isPresent()) return optionalUser.get();
-        else throw new IllegalArgumentException("유효하지 않은 회원입니다!");
+        else throw new ApolloException(INVALID_USER_ERROR, "존재하지 않는 회원입니다");
     }
     public User getUserByLogin(String login){
         Optional<User> optionalUser = userRepository.findByLogin(login);
         if (optionalUser != null && optionalUser.isPresent()) return optionalUser.get();
-        else throw new IllegalArgumentException("유효하지 않은 회원입니다!");
+        else throw new ApolloException(INVALID_USER_ERROR, "존재하지 않는 회원입니다");
     }
     public User getUserByName(String name){
         Optional<User> optionalUser = userRepository.findByName(name);
         if (optionalUser != null && optionalUser.isPresent()) return optionalUser.get();
-        else throw new IllegalArgumentException("유효하지 않은 회원입니다!");
+        else throw new ApolloException(INVALID_USER_ERROR, "존재하지 않는 회원입니다");
     }
 
 }
