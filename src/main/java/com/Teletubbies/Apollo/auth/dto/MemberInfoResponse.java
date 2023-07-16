a8b802eeb814464098bf9e40726dad0fd3a05ccd
dlawjddn <jwl5015@naver.com>
2023-07-16 16:29:23 +0900

    STYLE: member DTO 약간 수정 변동 사항 없음

--- src/main/java/com/Teletubbies/Apollo/auth/dto/MemberInfoResponse.java
+++ src/main/java/com/Teletubbies/Apollo/auth/dto/MemberInfoResponse.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.beans.ConstructorProperties;
+
 @Getter
 @NoArgsConstructor
 public class MemberInfoResponse {
