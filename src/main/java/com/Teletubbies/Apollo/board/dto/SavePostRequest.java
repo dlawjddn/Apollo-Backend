448e3df8b7424c4bd77f788d0448b6455ff7a9e5
dlawjddn <jwl5015@naver.com>
2023-08-14 14:47:06 +0900

    Feat: 게시글 저장 dto 필드 추가

--- src/main/java/com/Teletubbies/Apollo/board/dto/SavePostRequest.java
+++ src/main/java/com/Teletubbies/Apollo/board/dto/SavePostRequest.java
@@ -2,9 +2,12 @@ package com.Teletubbies.Apollo.board.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class SavePostRequest {
     private Long userId;
     private String title;
     private String content;
+    private List<String> tagNames;
 }
