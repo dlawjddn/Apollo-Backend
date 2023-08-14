0551380b0fa143f9cc7a90ed95ca03c560f50895
dlawjddn <jwl5015@naver.com>
2023-08-15 04:18:22 +0900

    Feat: PostWithTagRepository 로직 추가

--- src/main/java/com/Teletubbies/Apollo/board/repository/PostWithTagRepository.java
+++ src/main/java/com/Teletubbies/Apollo/board/repository/PostWithTagRepository.java
@@ -1,11 +1,14 @@
 package com.Teletubbies.Apollo.board.repository;
 
+import com.Teletubbies.Apollo.board.domain.Post;
 import com.Teletubbies.Apollo.board.domain.PostWithTag;
 import com.Teletubbies.Apollo.board.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostWithTagRepository extends JpaRepository<PostWithTag, Long> {
     List<PostWithTag> findAllByTag(Tag tag);
+    List<PostWithTag> findAllByPost(Post post);
 }
