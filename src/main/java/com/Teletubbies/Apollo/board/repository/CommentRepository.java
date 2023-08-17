e96809bb982304417e739358c5b59c648ac6c7be
dlawjddn <jwl5015@naver.com>
2023-08-18 02:46:30 +0900

    Feat: 댓글 수정용 조회 로직 추가

--- src/main/java/com/Teletubbies/Apollo/board/repository/CommentRepository.java
+++ src/main/java/com/Teletubbies/Apollo/board/repository/CommentRepository.java
@@ -6,8 +6,10 @@ import com.Teletubbies.Apollo.board.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findById(Long aLong);
     List<Comment> findALlByPost(Post post);
     List<Comment> findAllByApolloUser(ApolloUser apolloUser);
 }
