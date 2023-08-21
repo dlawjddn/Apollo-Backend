03358ba2390e77770085332ebdd49dbd212ffddd
dlawjddn <jwl5015@naver.com>
2023-08-21 19:56:49 +0900

    Fix: 게시글에 모든 정보 반환으로 변경, dto 필드 추가

--- src/main/java/com/Teletubbies/Apollo/board/dto/post/response/PostDetailResponse.java
+++ src/main/java/com/Teletubbies/Apollo/board/dto/post/response/PostDetailResponse.java
@@ -1,6 +1,8 @@
 package com.Teletubbies.Apollo.board.dto.post.response;
 
+import com.Teletubbies.Apollo.board.domain.Tag;
 import com.Teletubbies.Apollo.board.dto.comment.response.CommentInPostResponse;
+import com.Teletubbies.Apollo.board.dto.tag.ConvertTag;
 import lombok.Data;
 
 import java.util.List;
@@ -9,9 +11,11 @@ import java.util.List;
 public class PostDetailResponse {
     private FindPostResponse post;
     private List<CommentInPostResponse> comments;
+    private List<ConvertTag> tags;
 
-    public PostDetailResponse(FindPostResponse post, List<CommentInPostResponse> comments) {
+    public PostDetailResponse(FindPostResponse post, List<CommentInPostResponse> comments, List<ConvertTag> tags) {
         this.post = post;
         this.comments = comments;
+        this.tags = tags;
     }
 }
