c3fe2d916b04c7c755fa45bec1ef9fd2954d7def
dlawjddn <jwl5015@naver.com>
2023-08-22 16:15:15 +0900

    Fix: 게시글 수정 api 명세 변경에 따른 필드 삭제

--- src/main/java/com/Teletubbies/Apollo/board/dto/post/request/UpdatePostRequest.java
+++ src/main/java/com/Teletubbies/Apollo/board/dto/post/request/UpdatePostRequest.java
@@ -7,7 +7,6 @@ import java.util.List;
 @Data
 public class UpdatePostRequest {
     private Long userId;
-    private Long postId;
     private String title;
     private String content;
     private List<String> tagNames;
