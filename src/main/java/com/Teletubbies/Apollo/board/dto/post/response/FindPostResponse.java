46f6828f59f7a9f8270d7643759be52323cbef01
dlawjddn <jwl5015@naver.com>
2023-08-22 15:14:37 +0900

    Feat: 프론트에서 내용을 제거 하고 태그와 생성시간을 추가해달라고 함 그에 따른 수정

--- src/main/java/com/Teletubbies/Apollo/board/dto/post/response/FindPostResponse.java
+++ src/main/java/com/Teletubbies/Apollo/board/dto/post/response/FindPostResponse.java
@@ -1,18 +1,24 @@
 package com.Teletubbies.Apollo.board.dto.post.response;
 
+import com.Teletubbies.Apollo.board.dto.tag.ConvertTag;
 import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
 
 @Data
 public class FindPostResponse {
     private Long userId;
     private Long postId;
     private String title;
-    private String content;
+    private List<ConvertTag> tags;
+    private Date createAt;
 
-    public FindPostResponse(Long userId, Long postId, String title, String content) {
+    public FindPostResponse(Long userId, Long postId, String title, List<ConvertTag> tags, Date createAt) {
         this.userId = userId;
         this.postId = postId;
         this.title = title;
-        this.content = content;
+        this.tags = tags;
+        this.createAt = createAt;
     }
 }
