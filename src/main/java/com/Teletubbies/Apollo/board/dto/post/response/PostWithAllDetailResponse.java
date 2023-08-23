03178bc0fc373ef42f85b85fca4c54d4b0fcc923
dlawjddn <jwl5015@naver.com>
2023-08-24 01:24:45 +0900

    Style: dto 이름 구체적으로 명시

--- src/main/java/com/Teletubbies/Apollo/board/dto/post/response/PostWithAllDetailResponse.java
+++ src/main/java/com/Teletubbies/Apollo/board/dto/post/response/PostWithAllDetailResponse.java
@@ -1,18 +1,16 @@
 package com.Teletubbies.Apollo.board.dto.post.response;
 
-import com.Teletubbies.Apollo.board.domain.Tag;
 import com.Teletubbies.Apollo.board.dto.comment.response.CommentInPostResponse;
-import com.Teletubbies.Apollo.board.dto.tag.ConvertTag;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
-public class PostDetailResponse {
-    private OriginPostResponse post;
+public class PostWithAllDetailResponse {
+    private PostOnlyPostResponse post;
     private List<CommentInPostResponse> comments;
 
-    public PostDetailResponse(OriginPostResponse post, List<CommentInPostResponse> comments) {
+    public PostWithAllDetailResponse(PostOnlyPostResponse post, List<CommentInPostResponse> comments) {
         this.post = post;
         this.comments = comments;
     }
