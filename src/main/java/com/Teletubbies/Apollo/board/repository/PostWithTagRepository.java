1bb7addd2bac70c3d37b92c6220ee710df9b09c5
dlawjddn <jwl5015@naver.com>
2023-08-27 18:08:43 +0900

    Feat: tag 별 post 페이지네이션 조회 repository 로직 추가

--- src/main/java/com/Teletubbies/Apollo/board/repository/PostWithTagRepository.java
+++ src/main/java/com/Teletubbies/Apollo/board/repository/PostWithTagRepository.java
@@ -3,6 +3,8 @@ package com.Teletubbies.Apollo.board.repository;
 import com.Teletubbies.Apollo.board.domain.Post;
 import com.Teletubbies.Apollo.board.domain.PostWithTag;
 import com.Teletubbies.Apollo.board.domain.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.Optional;
 
 public interface PostWithTagRepository extends JpaRepository<PostWithTag, Long> {
     List<PostWithTag> findAllByTag(Tag tag);
+    Page<PostWithTag> findPostWithTagByTag(Tag tage, Pageable pageable);
     List<PostWithTag> findAllByPost(Post post);
     Optional<PostWithTag> findByPostAndTag(Post post, Tag tag);
     Long countByTag(Tag tag);
