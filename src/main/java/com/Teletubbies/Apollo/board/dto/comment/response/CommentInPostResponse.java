19afec0b97d62886e36bf3954c34e93710f962c3
dlawjddn <jwl5015@naver.com>
2023-08-28 14:39:56 +0900

    Feat: 댓글 관련 api 반환 데이터 필드 추가

--- src/main/java/com/Teletubbies/Apollo/board/dto/comment/response/CommentInPostResponse.java
+++ src/main/java/com/Teletubbies/Apollo/board/dto/comment/response/CommentInPostResponse.java
@@ -1,6 +1,7 @@
 package com.Teletubbies.Apollo.board.dto.comment.response;
 
 import lombok.Data;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Date;
 
@@ -8,12 +9,14 @@ import java.util.Date;
 public class CommentInPostResponse {
     private Long commentId;
     private Long writerId;
+    private String writerLogin;
     private String content;
     private Date writeAt;
 
-    public CommentInPostResponse(Long commentId, Long writerId, String content, Date writeAt) {
+    public CommentInPostResponse(Long commentId, Long writerId, String writerLogin, String content, Date writeAt) {
         this.commentId = commentId;
         this.writerId = writerId;
+        this.writerLogin = writerLogin;
         this.content = content;
         this.writeAt = writeAt;
     }
